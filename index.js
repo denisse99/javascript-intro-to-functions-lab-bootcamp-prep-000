9a6deef0f625bca1558db67f6d40df4279bc4466
Denisse Urdaneta <denisseurdaneta1999@hotmail.com>
2017-09-28 17:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,3 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
-  
-  if (string === "I love you, Grandma.") {
-    return "I love you, too."
-  }
